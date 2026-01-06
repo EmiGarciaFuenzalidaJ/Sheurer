01ebf7958b797ff070c980fd2d3a5e773356e56e
Emi Garcia Fuenzalida <emigarciafuenzalida@gmail.com>
2026-01-06 09:45:59 -0300

    contact section ready

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,31 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+$receiving_email_address = 'Dana@scheurerpropiedades.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+  include($php_email_form);
+} else {
+  die('Unable to load the "PHP Email Form" Library!');
+}
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// DESTINO
+$contact->to = $receiving_email_address;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// IMPORTANTE: usar un email del dominio como remitente
+$contact->from_name = 'Formulario Web - Scheurer';
+$contact->from_email = 'no-reply@scheurerpropiedades.com'; // debe existir o al menos el dominio
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// El mail del usuario se usa para responder
+$contact->reply_to = $_POST['email'];
 
-  echo $contact->send();
+$contact->subject = $_POST['subject'];
+
+// Mensajes
+$contact->add_message($_POST['name'], 'Nombre');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Mensaje', 10);
+
+echo $contact->send();
 ?>
